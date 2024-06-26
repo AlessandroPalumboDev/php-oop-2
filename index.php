0582e610432ab9f85adf973ceef2b13e0c207c79
Alessandro Palumbo <163033493+AlessandroPalumboDev@users.noreply.github.com>
2024-06-26 15:38:34 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+
+
+
+
+
+
+
+
+
+
+
+?>
+
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OOP Shop</title>
+    <!-- Boostrap css -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h1 class="text-center text-uppercase">OOP Shop</h1>
+                </div>
+            </div>
+        </div>
+    </header>
+
+
+
+    <!-- Boostrap js -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
